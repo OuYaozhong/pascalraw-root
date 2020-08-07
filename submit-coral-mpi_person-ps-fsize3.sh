a6b89bd0ceda2ce5a2858271dbe8ec01215aea98
Yiuzon <ou.yaozhong@connect.um.edu.mo>
2020-08-07 23:18:37 +0800

    make file suitable for ps mode

---
+++ submit-coral-mpi_person-ps-fsize3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name              person_ps-fsize3
+#SBATCH --partition             mpi-short
+#SBATCH --nodes                 1
+#SBATCH --tasks-per-node        1
+#SBATCH --time                  24:00:00
+#SBATCH --cpus-per-task         40
+#SBATCH --mem                   50G
+#SBATCH --output                person_ps-fsize3.%j.out
+#SBATCH --error                 person_ps-fsize3.%j.err
+#SBATCH --mail-type             ALL
+#SBATCH --mail-user             ou.yaozhong@connect.um.edu.mo
+
+source /etc/profile
+source /etc/profile.d/modules.sh
+
+#Adding modules
+module add openmpi/4.0.0/gcc/4.8.5
+
+ulimit -s unlimited
+
+#Your program starts here
+module load matlab/R2019a
+cd ~/pascalraw-ps-size3-person/voc-release5-raw
+matlab -nodisplay -r "addpath(genpath('~/pascalraw-ps-size3-person')); pascal('person',3);" -logfile ~/pascalraw-ps-size3-person/log/person_ps-fsize3.log
