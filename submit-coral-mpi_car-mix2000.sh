ac640f82a77617059ea1747b366ea9f060f2870a
Yiuzon <ou.yaozhong@connect.um.edu.mo>
2020-08-09 12:35:25 +0800

    edit the HPCC submit file to use mpi-normal instead of mpi-short; add .gitkeep to fix the leaky folder that is not tracked by git for empty content; manage the 98523 job's log file

--- submit-coral-mpi_car-mix2000.sh
+++ submit-coral-mpi_car-mix2000.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name              car_mix2000
-#SBATCH --partition             mpi-short
+#SBATCH --partition             mpi-normal
 #SBATCH --nodes                 1
 #SBATCH --tasks-per-node        1
-#SBATCH --time                  24:00:00
+#SBATCH --time                  72:00:00
 #SBATCH --cpus-per-task         40
 #SBATCH --mem                   50G
 #SBATCH --output                car_mix2000.%j.out
