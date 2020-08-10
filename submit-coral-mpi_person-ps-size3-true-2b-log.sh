ea2b94641bf836e3a35a4517a20217f8e7d8cbbb
Yiuzon <ou.yaozhong@connect.um.edu.mo>
2020-08-10 12:20:34 +0800

    edit HPCC submit file to adjust to ps-size true 2b-log mode

--- submit-coral-mpi_person-ps-size3-true-2b-log.sh
+++ submit-coral-mpi_person-ps-size3-true-2b-log.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name              person_true-2b-log
+#SBATCH --job-name              person_ps-size3-true-2b-log
 #SBATCH --partition             mpi-short
 #SBATCH --nodes                 1
 #SBATCH --tasks-per-node        1
 #SBATCH --time                  24:00:00
 #SBATCH --cpus-per-task         40
 #SBATCH --mem                   50G
-#SBATCH --output                person_true-2b-log.%j.out
-#SBATCH --error                 person_true-2b-log.%j.err
+#SBATCH --output                person_ps-size3-true-2b-log.%j.out
+#SBATCH --error                 person_ps-size3-true-2b-log.%j.err
 #SBATCH --mail-type             ALL
 #SBATCH --mail-user             ou.yaozhong@connect.um.edu.mo
 
@@ -21,5 +21,5 @@ ulimit -s unlimited
 
 #Your program starts here
 module load matlab/R2019a
-cd ~/pascalraw-true-2b-log-person/voc-release5-raw
-matlab -nodisplay -r "addpath(genpath('~/pascalraw-true-2b-log-person')); pascal('person',3);" -logfile ~/pascalraw-true-2b-log-person/log/person_true-2b-log.log
+cd ~/pascalraw-ps-size3-true-2b-log-person/voc-release5-raw
+matlab -nodisplay -r "addpath(genpath('~/pascalraw-ps-size3-true-2b-log-person')); pascal('person',3);" -logfile ~/pascalraw-ps-size3-true-2b-log-person/log/person-ps-size3-true-2b-log.log
