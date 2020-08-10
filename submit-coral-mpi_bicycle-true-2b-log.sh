c0d14c1e7524eee4cd5eb946a0dbb5d2d0453c47
Yiuzon <ou.yaozhong@connect.um.edu.mo>
2020-08-10 13:34:31 +0800

    edit HPCC submit files; manage successful job 98950's log files

--- submit-coral-mpi_bicycle-true-2b-log.sh
+++ submit-coral-mpi_bicycle-true-2b-log.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name              person_true-2b-log
+#SBATCH --job-name              car_true-2b-log
 #SBATCH --partition             mpi-short
 #SBATCH --nodes                 1
 #SBATCH --tasks-per-node        1
 #SBATCH --time                  24:00:00
 #SBATCH --cpus-per-task         40
 #SBATCH --mem                   50G
-#SBATCH --output                person_true-2b-log.%j.out
-#SBATCH --error                 person_true-2b-log.%j.err
+#SBATCH --output                car_true-2b-log.%j.out
+#SBATCH --error                 car_true-2b-log.%j.err
 #SBATCH --mail-type             ALL
 #SBATCH --mail-user             ou.yaozhong@connect.um.edu.mo
 
@@ -21,5 +21,5 @@ ulimit -s unlimited
 
 #Your program starts here
 module load matlab/R2019a
-cd ~/pascalraw-true-2b-log-person/voc-release5-raw
-matlab -nodisplay -r "addpath(genpath('~/pascalraw-true-2b-log-person')); pascal('person',3);" -logfile ~/pascalraw-true-2b-log-person/log/person_true-2b-log.log
+cd ~/pascalraw-true-2b-log-car/voc-release5-raw
+matlab -nodisplay -r "addpath(genpath('~/pascalraw-true-2b-log-car')); pascal('car',3);" -logfile ~/pascalraw-true-2b-log-car/log/car_true-2b-log.log
